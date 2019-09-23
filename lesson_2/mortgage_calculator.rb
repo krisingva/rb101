f1384a61103c248ef390a99f6e5130bdc58524d3
Kris Ingva <krisingva@gmail.com>
2019-09-23 11:00:01 -0400

    Mortgage calculator with yml

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -8,46 +8,43 @@
 # monthly interest rate
 # loan duration in months
 
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_messages.yml')
+
 def valid_integer?(num)
   num.to_i.to_s == num && num.to_i > 0
 end
 
-def valid_float?(num)
-  num.to_f.to_s == num && num.to_f > 0
+def valid_number?(num)
+  num.to_f.to_s == num || num.to_i.to_s == num && num.to_f > 0.0
 end
 
-puts "> Welcome to the mortgage calculator!"
+puts MESSAGES['welcome']
 
 loop do # start of main loop
   loan = ''
   loop do
-    puts "> Please insert loan amount in USD:"
+    puts MESSAGES['loan_amount']
     loan = gets.chomp
 
-    if valid_integer?(loan)
-      loan = loan.to_i
-      break
-    elsif valid_float?(loan)
+    if valid_number?(loan)
       loan = loan.to_f
       break
     else
-      puts "> This is an invalid number."
+      puts MESSAGES['invalid_number']
     end
   end
 
   apr = ''
   loop do
-    puts "> Please insert Annual Percentage Rate in %:"
+    puts MESSAGES['apr']
     apr = gets.chomp
 
-    if valid_integer?(apr)
-      apr = apr.to_i
-      break
-    elsif valid_float?(apr)
+    if valid_number?(apr)
       apr = apr.to_f
       break
     else
-      puts "> This is an invalid number."
+      puts MESSAGES['invalid_number']
     end
   end
 
@@ -56,38 +53,31 @@ loop do # start of main loop
 
   loan_in_years = ''
   loop do
-    puts "> Please enter loan duration in years:"
+    puts MESSAGES['loan_duration']
     loan_in_years = gets.chomp
 
     if valid_integer?(loan_in_years)
       loan_in_years = loan_in_years.to_i
       break
-    elsif valid_float?(loan_in_years)
-      loan_in_years = loan_in_years.to_f
-      break
     else
-      puts "> This is an invalid number."
+      puts MESSAGES['integers_only']
     end
   end
 
   # loan in years converted to loan in months
   loan_in_months = loan_in_years * 12
 
-  p loan
-  p mpr
-  p loan_in_months
-
   # You can use the following Mortgage Calculator Formula:
   monthly_pay = loan * (mpr / (1 - (1 + mpr)**(-loan_in_months)))
   monthly_pay = monthly_pay.truncate(2)
   puts "> This is your monthly payment: $#{monthly_pay}"
 
-  puts "Would you like to do another calculation? (Y/N)"
+  puts MESSAGES['another_calculation']
   answer = gets.chomp
 
   break if answer.downcase.start_with?("n")
 end # end of main loop
 
-puts "Thank you for using the mortgage calculator. I hope it was helpful!"
+puts MESSAGES['exit_message']
 
 # Finally, don't forget to run your code through Rubocop.
